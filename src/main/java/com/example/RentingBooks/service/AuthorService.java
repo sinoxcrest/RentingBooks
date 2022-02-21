b42310ccf0024123c2c6bfb9ee88c871236ec6c3
sinoxcrest <sinoxcrest@gmail.com>
2022-02-21 19:24:51 +0545

    Book html initialize

--- src/main/java/com/example/RentingBooks/service/AuthorService.java
+++ src/main/java/com/example/RentingBooks/service/AuthorService.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface AuthorService {
     AuthorDto create(AuthorDto authorDto);
+
     List<AuthorDto> findAll();
+
     AuthorDto findById(Integer id);
 }
--- src/main/java/com/example/RentingBooks/service/CategoryService.java
+++ src/main/java/com/example/RentingBooks/service/CategoryService.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface CategoryService {
     CategoryDto create(CategoryDto categoryDto);
+
     List<CategoryDto> findAll();
+
     CategoryDto findById(Integer id);
 }
